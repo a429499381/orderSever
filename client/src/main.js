d27a8b6316965c4968c31ce6b768ead7b4a89685
7070m <cc@7070ms-iMac.local>
2020-05-01 21:42:26 +0800

    使用  vant  element-ui 不支持移动端

--- client/src/main.js
+++ client/src/main.js
@@ -13,7 +13,11 @@ axios.defaults.baseURL='http://localhost:3000/api/users';
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import animated from 'animate.css' // npm install animate.css --save安装，再引入
+import Vant from 'vant';
+import 'vant/lib/index.css';
+
 Vue.config.productionTip = false
+Vue.use(Vant);
 Vue.use(ElementUI)
 Vue.use(animated)
 Vue.use(VueAxios, axios)
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
-  <div  class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld  msg="Welcome to Your Vue.js App"/>
+  <div class="home">
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px;">
+        <van-button round block type="info" native-type="submit">登陆</van-button>
+      </div>
+    </van-form>
   </div>
 </template>
 
-<script>  
+<script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      username: "xutao",
+      password: ""
+    };
+  },
+  methods: {
+    onSubmit(values) {
+      console.log("submit", values);
+    }
   }
-}
+};
 </script>
