24218cfab95a75613370f0ef8fcfc4ca42538697
7070m <cc@7070ms-iMac.local>
2020-05-05 00:55:10 +0800

    vant 语法

---
+++ client/src/components/nav.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav class="nav">
+    <van-row type='flex' justify="space-around">
+      <van-col span="6">1</van-col>
+      <van-col span="6">2</van-col>
+    </van-row>
+
+    <van-row>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+      <van-col span="8">span: 8</van-col>
+    </van-row>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "head-nav"
+};
+</script>
+ 
+<style scoped>
+</style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,27 +1,6 @@
 <template>
   <div class="home">
-    <van-form @submit="onSubmit">
-      <van-field
-        v-model="username"
-        name="用户名"
-        label="用户名"
-        placeholder="用户名"
-        :rules="[{ required: true, message: '请填写用户名' }]"
-      />
-      <van-field
-        v-model="password"
-        type="password"
-        name="密码"
-        label="密码"
-        placeholder="密码"
-        :rules="[{ required: true, message: '请填写密码' }]"
-      />
-      <div style="margin: 16px;" class='submit'>
-        <van-button round block type="info" native-type="submit('login')">登陆</van-button>
-        <van-button round block type="info" native-type="submit('reg')">注册</van-button>
-      </div>
-    </van-form>
-      <van-loading v-if='isLoading' size="24px" vertical>加载中...</van-loading>
+    <Nav></Nav>
   </div>
 </template>
 
@@ -29,32 +8,16 @@
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue' 
 // import {Loading } from 'vant'  
-
+import Nav from '../components/nav.vue';
 export default {
   name: "Home",
-  components: {},
-  data() {
-    return {
-      username: "xutao",
-      password: "11",
-      isLoading: true,
-    };
+  components: {
+    Nav
   },
-  methods: {
-    onSubmit(values) {
-      console.log("submit", values);
-
-      // console.log('loading', Loading)
-  
-    }
-  }
 };
 </script>
 <style  scoped>
-  .submit {
-    display : flex;
-  }
-  .van-button:nth-child(1) {
-    margin-right: 10px; 
+  .home {
+    flex: 1
   }
 </style>
